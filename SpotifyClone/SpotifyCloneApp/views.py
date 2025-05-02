4617694677507b0b75a3fe31ba2a74db4ee274ec
EMMMABK <adilbikiev2@gmail.com>
2025-05-02 09:16:26 +0600

    add Update

--- SpotifyClone/SpotifyCloneApp/views.py
+++ SpotifyClone/SpotifyCloneApp/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 # Create your views here
 def index(request):
     return render(request, 'SpotifyCloneApp/index.html')
+
+def login(request):
+    return render(request, 'SpotifyCloneApp/login.html')
+
+
+def signup(request):
+    return render(request, 'SpotifyCloneApp/signup.html')
