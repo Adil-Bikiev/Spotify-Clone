019a76e9048bc8fe5971e897c9af59d2360eccc8
EMMMABK <adilbikiev2@gmail.com>
2025-05-03 12:49:24 +0600

    add Main Page

--- SpotifyClone/SpotifyCloneApp/urls.py
+++ SpotifyClone/SpotifyCloneApp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),        
+    path('logout/', views.logout, name='logout'),
+    path('user_in/', views.user_in, name='user_in')        
 ]
--- SpotifyClone/SpotifyCloneApp/views.py
+++ SpotifyClone/SpotifyCloneApp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.models import User
-from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth.models import User, auth
 # Create your views here
 def index(request):
     return render(request, 'SpotifyCloneApp/index.html')
@@ -17,12 +16,27 @@ def signup(request):
         password2 = request.POST['password2']
 
         if password == password2:
-            print('password same')
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Данный Email уже занят!')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Данный Username уже занят!')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+                return redirect('user_in')
         else:
             messages.info(request, 'Пароли не совпадают!')
             return redirect('signup')
     else:
         return render(request, 'SpotifyCloneApp/signup.html')
+
+def user_in(request):
+    return render(request, 'SpotifyCloneApp/user_in.html')
 
 def logout(request):
     pass
