5e1ff0d595d033b366559cd3a14e2f30405e7911
EMMMABK <adilbikiev2@gmail.com>
2025-05-03 12:30:51 +0600

    add Validation Password

--- SpotifyClone/SpotifyCloneApp/views.py
+++ SpotifyClone/SpotifyCloneApp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import login as auth_login, authenticate
 # Create your views here
 def index(request):
     return render(request, 'SpotifyCloneApp/index.html')
@@ -8,7 +10,19 @@ def login(request):
     return render(request, 'SpotifyCloneApp/login.html')
 
 def signup(request):
-    return render(request, 'SpotifyCloneApp/signup.html')
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            print('password same')
+        else:
+            messages.info(request, 'Пароли не совпадают!')
+            return redirect('signup')
+    else:
+        return render(request, 'SpotifyCloneApp/signup.html')
 
 def logout(request):
     pass
